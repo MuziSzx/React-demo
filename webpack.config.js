aeab7f777003f36c30ac63e582f319f81a0e1b33
shanzx <425002410@qq.com>
2019-01-24 10:32:27 +0800

    配置webpack环境

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+//向外暴露一个打包的配置对象，    因为webpack是基于node构建的，所以webpack支持所有的NODe api和语法
+
+module.exports = {
+    mode:'development',
+    // entry:'./src/main.js'
+    //在webpack 4、x 中，有一个很大的问题，就是约定大于配置，  约定，默认的打包入口路径是 src -->index.js
+}
+
+
+
+// 如果chorme浏览器支持哪些，则node就支持这些
